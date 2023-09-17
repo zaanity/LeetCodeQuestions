69ad30070ea642dec38f6753ea943351411a7772
Vaibhav <129985767+zaanity@users.noreply.github.com>
2023-09-17 17:25:31 +0530

    Added solution - LeetHub

---
+++ Print first n Fibonacci Numbers - GFG/print-first-n-fibonacci-numbers.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+
+class Solution
+{
+    public:
+    //Function to return list containing first n fibonacci numbers.
+    
+    
+    vector<long long> printFibb(int n) 
+    {
+        vector<long long> fibonacci(n);
+        fibonacci[0]=1;
+        fibonacci[1]=1;
+        
+        for(int i=2;i<n;i++){
+            fibonacci[i]=fibonacci[i-1]+fibonacci[i-2];
+        }
+        
+        return fibonacci;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+     //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking number of elements
+        int n;
+        cin>>n;
+        Solution obj;
+        //calling function printFibb()
+        vector<long long> ans = obj.printFibb(n);
+        
+        //printing the elements of vector
+        for(long long i:ans)cout<<i<<' ';
+        cout<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
